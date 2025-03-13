f40d8fcc11b14d4fa98cc46ab66356048fe53e75
Malin1601 <malin.134@live.no>
2025-03-13 18:10:44 +0100

    Update Fireguard_API.py

--- Fireguard_API.py
+++ Fireguard_API.py
@@ -16,8 +16,9 @@ if __name__ == "__main__":
     obs_delta = datetime.timedelta(days=2)
 
     wd = frc.get_weatherdata_now(location, obs_delta)
-    print (wd)
+    print(wd)
 
     predictions = frc.compute_now(location, obs_delta)
 
     print(predictions)
+    
