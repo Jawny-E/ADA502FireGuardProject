64cabc59307d874a32547d44d0fcc58d1043e0a0
Jawny-E <egrov.johnsen@gmail.com>
2025-03-17 22:14:24 +0100

    ci: fixes line-length settings, and fixes compliance
    Line-length is now set to 120 which I belive is standard, also cleaned up the other errors thrown by database.py

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://[USERNAME:PASSWORD]@fireguardproject.ggfqm.mongodb.net/')
 db = client['sample_mflix']
 
+
 # Check if 'users' collection exists
 def collection_exists(db, collection_name):
     try:
@@ -17,6 +18,7 @@ def collection_exists(db, collection_name):
 
     return collection_name in collections
 
+
 # Check if 'users' collection exists
 if collection_exists(db, 'users'):
     users_collection = db['users']
@@ -25,10 +27,10 @@ else:
     print("The 'users' collection does not exist.")
 
 
-
 def create_user(user_data):
     result = users_collection.insert_one(user_data)
     return result.inserted_id
+
 
 def get_user(user_id):
     return users_collection.find_one({"_id": ObjectId(user_id)})
@@ -37,4 +39,3 @@ def get_user(user_id):
 user = get_user("59b99db4cfa9a34dcd7885b7")
 
 print(user.get("name"))
-    
