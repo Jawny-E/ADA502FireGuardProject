99d6529c071c68a8b05ea136aa720ce93d6b26d4
AuroraSaetran <aurora@saetran.com>
2025-04-24 10:55:30 +0200

    "oppdatert med filer"

--- API/fireguard_app/app.py
+++ API/fireguard_app/app.py
@@ -1,6 +1,7 @@
 # The main application controlling the FastAPI
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, status
 from .Fireguard_API import get_fire_risk, get_fire_risk_trends
+from .kc.auth import verify_admin_role, verify_user_role, verify_sadmin_role, verify_suser_role, verify_user_path, verify_user_locquery
 
 
 # For å starte serveren : "poetry run uvicorn app:app --reload" i terminal
@@ -32,3 +33,36 @@ def fire_risk_endpoint(location: str, days_past: int = 7, weatherdata: bool = Fa
 @app.get("/locations/{location}/trends")
 def fire_risk__trends_endpoint(location: str):
     return get_fire_risk_trends(location)
+
+
+@app.get('/api/v1/admin')
+def protected_admin(admin: bool = Depends(verify_admin_role)):
+    return {"message": "This is a protected resource for ADMIN role."}
+
+
+@app.get('/api/v1/admin/service')
+def protected_sadmin(admin: bool = Depends(verify_sadmin_role)):
+    return {"message": "This is a protected resource for APP_ADMIN role."}
+
+
+@app.get('/api/v1/protected')
+def protected_user(user: bool = Depends(verify_user_role)):
+    return {"message": "This is a protected resource for USER role."}
+
+
+@app.get('/api/v1/protected/service')
+def protected_suser(user: bool = Depends(verify_suser_role)):
+    return {"message": "This is a protected resource for APP_USER role."}
+
+
+@app.get('/api/v1/public', status_code=status.HTTP_200_OK)
+def public_user():
+    return {"message": "This is a public resource for everyone."}
+
+@app.get("/api/v1/")
+def protected_user_query(user: bool = Depends(verify_user_locquery)):
+    return {"message": "This is a protected resource for any user that is registered on a location."}
+
+@app.get("/api/v1/{location}")
+def protected_user_loc(location: str, user: bool = Depends(verify_user_path)):
+    return {"message": f'This is a protected resource for any user that is registered on location = {location}.'}
